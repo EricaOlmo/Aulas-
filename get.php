29ebade7fcee85ce1e7c0696d7532afb069d54f4
Erica <ericaborges97@hotmail.com>
2018-05-23 14:52:51 -0300

    testando commit

---
+++ get.php
@@ -0,0 +1,11 @@
+<div style="text-align:center;">
+<?php
+
+	// Recebe os dados e guarda-os em variáveis
+	$nome = $_GET['nome'];
+	$email = $_GET['email'];
+
+	echo "SEU NOME: $nome / E-MAIL: $email";
+
+?>
+</div>
---
+++ post.php
@@ -0,0 +1,11 @@
+<div style="text-align:center;">
+<?php
+
+	// Recebe os dados e guarda-os em variáveis
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+
+	echo "SEU NOME: $nome / E-MAIL: $email";
+
+?>
+</div>
